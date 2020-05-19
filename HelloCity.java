3a5c50e0fe1611e38090a5e8b2dd2606eace2592
seongrog <seongrog@email.arizona.edu>
2020-05-19 16:07:43 -0700

    Update HelloCity.java

--- HelloCity.java
+++ HelloCity.java
@@ -3,7 +3,7 @@ public class HelloCity {
     public static void main(String[] args) {
         for (int i=0; i < args.length; i++)
 		{
-			System.out.println(args[i];
+			System.out.println(args[i]);
 		}
     }
 
